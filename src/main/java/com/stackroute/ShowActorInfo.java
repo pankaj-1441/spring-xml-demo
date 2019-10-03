7db7221b42a09759a3ba431dcc4194d0e184b932
pankaj-1441 <pankaj.chauhan.1441@gmail.com>
2019-10-03 11:05:54 +0530

    Task-6

--- src/main/java/com/stackroute/ShowActorInfo.java
+++ src/main/java/com/stackroute/ShowActorInfo.java
@@ -1,7 +1,5 @@
 package com.stackroute;
 
-import com.stackroute.domain.BeanLifeCycleDemoBean;
-import com.stackroute.domain.Movie;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,7 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ShowActorInfo {
     public static void main(String[] args) {
         ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
-        ((AbstractApplicationContext)context).registerShutdownHook();
+
 
     }
 }
