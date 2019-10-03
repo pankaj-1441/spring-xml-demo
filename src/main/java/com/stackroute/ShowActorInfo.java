87c98f9885ba9906101824003da500471bdd4240
pankaj-1441 <pankaj.chauhan.1441@gmail.com>
2019-10-03 10:19:42 +0530

    Task-5

--- src/main/java/com/stackroute/ShowActorInfo.java
+++ src/main/java/com/stackroute/ShowActorInfo.java
@@ -1,13 +1,15 @@
 package com.stackroute;
 
+import com.stackroute.domain.BeanLifeCycleDemoBean;
 import com.stackroute.domain.Movie;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ShowActorInfo {
     public static void main(String[] args) {
         ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie=context.getBean("movie",Movie.class);
+        ((AbstractApplicationContext)context).registerShutdownHook();
 
     }
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -7,11 +7,8 @@ import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
-public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
+public class Movie {
     private Actor actor;
-    private ApplicationContext context = null;
-    private BeanFactory beanFactory = null;
-    private String beanName = null;
 
 
     public void setActor(Actor actor) {
@@ -22,29 +19,7 @@ public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNam
         actor.actorInfo();
     }
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.context = applicationContext;
-        System.out.println("ApplicationContext has been set to your object");
-    }
 
 
-    @Override
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        this.beanFactory = beanFactory;
-        System.out.println("BeanFactory object has been injected");
-
-    }
-
-    @Override
-    public void setBeanName(String s) {
-        this.beanName = s;
-        System.out.println("bean name of your object is "+this.beanName);
-
-    }
-
-    public void displayDetails() {
-
-    }
 
 }
